abbed6a227ba46694a18d4c39038d292be2fb92e
coveraver <sivolapenko.valeriy@gmail.com>
2020-03-05 14:14:16 +0300

    select sudo command with args and no

--- main.py
+++ main.py
@@ -11,19 +11,20 @@ print(bot.get_me())
 @bot.message_handler(regexp='^![a-z]')
 def handle_message(message: Message):
     user_id, command = message.from_user.id, message.text.lower()
-    print(user_id)
     bot.delete_message(message.chat.id, message.message_id)
     try:
         result = commands_dict.light_commands[command]
         if result:
             bot.reply_to(message.reply_to_message, text=result(), parse_mode='markdown', disable_web_page_preview=True)
     except (AttributeError, KeyError):
-        if user_id == '12345':
+        if user_id == 169603089:
             try:
                 result = commands_dict.sudo_commands[command]
-                if result:
+                if result and result == '!wall':
                     bot.reply_to(message.reply_to_message, text=result(), parse_mode='markdown',
                                  disable_web_page_preview=True)
+                elif result:
+                    pass
             except (AttributeError, KeyError):
                 pass
 
