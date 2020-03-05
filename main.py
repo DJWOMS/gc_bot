12619e61200461cd431796fdf288ef2ed793c5fb
coveraver <sivolapenko.valeriy@gmail.com>
2020-03-05 13:42:14 +0300

    add exception to KeyError

--- main.py
+++ main.py
@@ -8,16 +8,16 @@ bot = telebot.TeleBot(BOT_TOKEN)
 print(bot.get_me())
 
 
-@bot.message_handler(regexp='^![a-z]$+')
+@bot.message_handler(regexp='^![a-z]$')
 def handle_message(message: Message):
     user_id, command = message.from_user.id, message.text.lower()
     bot.delete_message(message.chat.id, message.message_id)
-    result = commands_dict.light_commands[command]
-    if result:
-        try:
+    try:
+        result = commands_dict.light_commands[command]
+        if result:
             bot.reply_to(message.reply_to_message, text=result(), parse_mode='markdown', disable_web_page_preview=True)
-        except AttributeError:
-            pass
+    except (AttributeError, KeyError):
+        pass
 
 
 bot.polling()
