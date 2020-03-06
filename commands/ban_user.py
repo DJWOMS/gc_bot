a1e28cbaa2111b82bc063613a206f7d1286ba82e
coveraver <sivolapenko.valeriy@gmail.com>
2020-03-06 14:52:33 +0300

    refactor ban in db

--- commands/ban_user.py
+++ commands/ban_user.py
@@ -8,7 +8,7 @@ def ban_user(username: Message, user: User, reason: Message):
     banned_user = [getattr(user, attr) for attr in dir(user) if attr in attrs]
     b_user = ' '.join([i for i in banned_user if i is not None])
     user.is_banned = True
-    ban_user_db(user)
+    ban_user_db(user=user)
     if reason:
         reason = ' '.join(reason.text.split(' ')[1:])
         return f'*{username} забанил пользавателя {b_user}\nПричина:*\n`{reason}`'
