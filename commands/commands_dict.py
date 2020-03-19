64078976206f7dd9e8d74daddcb9b066b6cdaf0c
coveraver <sivolapenko.valeriy@gmail.com>
2020-03-19 11:15:28 +0300

    create auto import functions from views inner modules

--- commands/commands_dict.py
+++ commands/commands_dict.py
@@ -1,20 +1,35 @@
 """Light commands available to all users. But sudo only admin or sudoers"""
+from typing import Sequence, Dict, Callable
 
-from commands import *
-
-
-light_commands = {
-    '!paste': share_process,
-    '!web': web_process,
-    '!tut': tut_process,
-    '!wq': wq_process,
-    '!flood': flood_process,
-}
+from views import *
+import views
 
 
-sudo_commands = {
-    '!ban': ban_process,
-    '!warn': warn_process,
-    '!sudo': sudo_process,
-    '!unban': unban_process,
-}
+def init_light_command(*commands: Sequence[str]) -> Dict[str, Callable]:
+    light_command = {}
+    for command in commands:
+        for view in views.__all__:
+            if command == view.split('_')[0]:
+                try:
+                    light_command[f'!{command}'] = getattr(globals()[view], view)
+                except AttributeError as err:
+                    print(
+                        f'{err}. '
+                        f'May be you forget add or rename function. Module and function name must be the same name!'
+                    )
+    return light_command
+
+
+def init_sudo_command(*commands: Sequence[str]) -> Dict[str, Callable]:
+    sudo_command = {}
+    for command in commands:
+        for view in views.__all__:
+            if command == view.split('_')[0]:
+                try:
+                    sudo_command[f'!{command}'] = getattr(globals()[view], view)
+                except AttributeError as err:
+                    print(
+                        f'{err}. '
+                        f'May be you forget add or rename function. Module and function name must be the same name!'
+                    )
+    return sudo_command
--- views/share_process.py
+++ views/share_process.py
@@ -6,7 +6,7 @@ def share_process(message: Message, bot: telebot) -> Message:
     """Links to share code web stages"""
     msg = """
     Для того, чтобы поделиться `кодом` или `ошибкой`, необходимо воспользоваться онлайн сервисами, такими как:\n
-*-* https://hastebin.com/\n*-* https://pastebin.com/\n*-* https://codeshare.io/
+*+* https://hastebin.com/\n*+* https://pastebin.com/\n*+* https://codeshare.io/
     """
     return bot.reply_to(message.reply_to_message, text=msg, parse_mode='markdown',
                         disable_web_page_preview=True)
